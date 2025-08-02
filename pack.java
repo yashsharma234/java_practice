8267c759ac1804c5f545583dff18a7566c6dc2d9
yashsharma234 <yashsh672@gmail.com>
2025-08-02 16:04:32 +0530

    java

---
+++ pack.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+
+public class pack{
+    public static void main(String[] args) {
+        
+        Scanner sc =new Scanner(System.in);
+
+        String name = sc.nextLine();//nextFloat,nextInt
+
+        System.out.println(name);
+    }
+   
+
+  
+
+
+
+}
---
+++ variable.java
@@ -0,0 +1,15 @@
+public class variable {
+    public static void main(String[]args){
+        //variables
+        int a=10;
+        int b=20;
+        int sum=a+b;
+        int difference=b-a;
+    
+        System.out.println(sum);
+        //int difference=b-a;
+        System.out.print(difference);
+    }
+    
+    
+}
