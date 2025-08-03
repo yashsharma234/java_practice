13a3ce26138b2cf08bfd97599ababbe14d33800e
yashsharma234 <yashsh672@gmail.com>
2025-08-03 11:01:58 +0530

    new

---
+++ Loops.java
@@ -0,0 +1,31 @@
+public class Loops{
+
+    public static void main(String args[]){
+    
+
+       // for(int i=0;i<10;i+=1){//we can write i++ or i=i+1
+
+             //System.out.println("yash");
+          //    System.out.println(i);
+        //}
+
+
+       // int i=0;
+       // while(i<11){
+
+          //  System.out.println(i);
+          //  i+=1;
+       // }
+      
+
+       int i=0;
+       do { 
+          System.out.println(i); 
+          i+=1;
+       } while (i<11);
+
+
+
+    }
+    
+}
---
+++ conditional.java
@@ -0,0 +1,41 @@
+import java.util.*;
+
+public class conditional {
+   public static void main(String args[]){
+
+       Scanner sc=new Scanner(System.in);
+
+     //  int age=sc.nextInt();
+
+       //if(age>18){
+      // System.out.print("adult");
+
+
+      //} else{
+
+      //  System.out.print("Not Adult");
+      //}
+       int button=sc.nextInt();
+
+       switch(button){
+
+           case 1:System.out.println("hello");
+           break;
+
+           case 2:System.out.println("namaste");
+           break;
+
+           case 3:System.out.println("bonjour");
+           break;
+
+           default:System.out.println("invalid");
+
+
+        }
+
+
+
+    }
+
+    
+}
